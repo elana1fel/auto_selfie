8f7b2760a968155c7d6837fa711229c615ea84f7
elana1fel <elana1fel@gmail.com>
2021-04-03 22:52:45 +0300

    fixed bugs in run_me

--- code/run_me.py
+++ code/run_me.py
@@ -2,28 +2,41 @@ from imutils.video import VideoStream, FPS
 import numpy as np
 import time
 import cv2
-import selfie_utils
+#import selfie_utils
 
-RUNNING_MODE = CAMERA
 
-def run():
+def run(video_path=None):
+    if video_path is None: #we are running on camera mode
+        running_mode = 'camera'
+    else:
+        running_mode = 'video'
+
     counter = 0
     last_taken_selfie = None
     show_stats = False
     draw_contours = False
 
     print("starting looking for perfect selfie mode")
-    if RUNNING_MODE == 'CAMERA':
+    if video_path is None: #we are running on camera mode
         vs = VideoStream(src=0).start()
         time.sleep(1.0)
         fps= FPS().start()
-    cv2.namedWindow(RUNNING_MODE)
+
+    elif running_mode == 'video':
+        cap = cv2.VideoCapture(video_path)
+
+    cv2.namedWindow(running_mode)
 
     while True:
-        frame = vs.read()
+        if running_mode == 'camera':
+            frame = vs.read()
+        else:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
         #gray_img = selfie_utils.edit_img(frame)
         mar = 5 #TODO TEMP        
-
 
         if draw_contours:
             cv2.drawContours(frame, [mouth_hull], -1, (0, 255, 0), 1)
@@ -32,9 +45,9 @@ def run():
             cv2.putText(frame, "MAR: {}".format(mar), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
         if mar <= .3 or mar > .38 :
-            COUNTER += 1
+            counter += 1
 
-            if COUNTER >= 5: #we need to check it
+            if counter >= 5: #we need to check it
                 if last_taken_selfie is not None:
                     # check if there is a difference between current img to last taken selfie.
                     # if it is the same go back to while loop
@@ -52,14 +65,13 @@ def run():
         if key2 == ord('q'):
             break
 
-    if RUNNING_MODE = 'CAMERA':
+    if running_mode == 'camera':
         fps.stop()
+        vs.stop()
 
 
     cv2.destroyAllWindows()
 
-    if RUNNING_MODE = 'CAMERA':
-        vs.stop()
 
 
 if __name__ == "__main__":
