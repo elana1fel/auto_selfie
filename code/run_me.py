bef956f5bf401c6c33ef94a2f48c07693bf39d98
elana1fel <elana1fel@gmail.com>
2021-04-04 00:35:29 +0300

    added ear calculation

--- code/run_me.py
+++ code/run_me.py
@@ -13,8 +13,8 @@ def run(video_path=None):
 
     counter = 0
     last_taken_selfie = None
-    show_stats = False
-    draw_contours = False
+    show_stats = True
+    draw_contours = True
 
 
     print("starting looking for perfect selfie mode")
@@ -36,35 +36,37 @@ def run(video_path=None):
                 break
 
         gray_img = selfie_utils.edit_img(frame)
-        mar, mouth_hull = selfie_utils.detect_face(gray_img) 
+        faces = selfie_utils.detect_face(gray_img) 
 
-        if draw_contours:
-            cv2.drawContours(frame, [mouth_hull], -1, (0, 255, 0), 1)
-    
-        if show_stats:
-            cv2.putText(frame, "MAR: {}".format(mar), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        for face in faces:
+            if draw_contours:
+                for face_part in ['mouth_hull', 'l_eye_hull', 'r_eye_hull']:
+                    cv2.drawContours(frame, [face[face_part]], -1, (0, 255, 0), 1)
+        
+            if show_stats:
+                cv2.putText(frame, "MAR: {}".format(face['mar']), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-        if mar <= .3 or mar > .38 :
-            counter += 1
+            if face['mar'] <= .3 or face['mar'] > .38 :
+                counter += 1
 
-            if counter >= 5: #we need to check it
-                if last_taken_selfie is not None:
-                    # check if there is a difference between current img to last taken selfie.
-                    # if it is the same go back to while loop
-                    pass
+                if counter >= 5: #we need to check it
+                    if last_taken_selfie is not None:
+                        # check if there is a difference between current img to last taken selfie.
+                        # if it is the same go back to while loop
+                        pass
 
-                else:
-                    # first selfie
-                    # TODO:take a selfie
-                    pass
+                    else:
+                        # first selfie
+                        # TODO:take a selfie
+                        pass
 
         cv2.imshow("Frame", frame)
         if running_mode == 'camera':
             fps.update()
 
-        key2 = cv2.waitKey(1) & 0xFF
-        if key2 == ord('q'):
-            break
+        #key2 = cv2.waitKey(1) & 0xFF
+        #if key2 == ord('q'):
+        #    break
 
     if running_mode == 'camera':
         fps.stop()
--- code/selfie_utils.py
+++ code/selfie_utils.py
@@ -17,22 +17,42 @@ def edit_img(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return gray
 
-def smile(mouth):
+def mouth_aspect_ratio(mouth):
     '''
 
     '''
     A = dist.euclidean(mouth[3], mouth[9])
     B = dist.euclidean(mouth[2], mouth[10])
     C = dist.euclidean(mouth[4], mouth[8])
-    avg = (A + B + C)/3
+    avg = (A + B + C) / 3
     D = dist.euclidean(mouth[0], mouth[6])
     mar = avg/D
-    return mar
+    return mar #return the mouth aspect ratio
+
+def eye_aspect_ratio(eye):
+    '''
+
+    '''
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
 
 def detect_face(np_img):
     '''
 
     '''
+    faces = []
     shape_predictor= "code/dat_files/shape_predictor_68_face_landmarks.dat" #dace_landmark
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(shape_predictor)
@@ -42,17 +62,26 @@ def detect_face(np_img):
 
     rects = detector(np_img, 0)
     for rect in rects:
-        print("running on:  ", rect)
         shape = predictor(np_img, rect)
         shape = face_utils.shape_to_np(shape)
         mouth= shape[mouth_start:mouth_end]
         l_eye = shape[l_eye_start:l_eye_end]
         r_eye = shape[r_eye_start:r_eye_end]
-        mar = smile(mouth)
-        mouth_hull = cv2.convexHull(mouth)
-        print(mar)
-    return mar, mouth_hull #TODO check if eyes open
+        mar = mouth_aspect_ratio(mouth)
+        l_ear = eye_aspect_ratio(l_eye)
+        r_ear = eye_aspect_ratio(r_eye)
+        faces.append({'mar': mar,
+                      'mouth_hull' : cv2.convexHull(mouth),
+                      'l_ear' : l_ear, 
+                      'l_eye_hull' : cv2.convexHull(l_eye),
+                      'r_ear' : r_ear,
+                      'r_eye_hull' : cv2.convexHull(r_eye)})
+    return faces
 
+def show_frame(np_img):
+    from PIL import Image
+    img = Image.fromarray(np_img)
+    img.show()
 
 
 
