2b947f7cd3f0ddb56849839fb78086b32a026c2a
Ofer <oferg90@gmail.com>
2021-05-18 18:58:43 +0300

    use eyes and face code, add own mouth

--- code/haarCascade.py
+++ code/haarCascade.py
@@ -11,17 +11,17 @@ last_taken_selfie = None
 total = 0
 counter = 0
 
-def takeSelfies(eye, smile,frame, grayscaleFrame, output_folder, cartoon, pencil, useCanny=False):
+def takeSelfies(eyes, smile,frame, gray, output_folder, cartoon, pencil, useCanny=False):
 
     global total, counter, last_taken_selfie
 
-    if ((eye is not None) and (smile is not None ) and (len(eye)>0) and (len(smile)>0)):
+    if ((eyes is not None) and (smile is not None ) and (len(eyes)>0) and (len(smile)>0)):
         counter += 1
 
         # print(f"counter is: {counter}")
         if counter >= 5:  # we need to check it
             if last_taken_selfie is not None:
-                (score, diff) = compare_ssim(last_taken_selfie, grayscaleFrame, full=True)
+                (score, diff) = compare_ssim(last_taken_selfie, gray, full=True)
                 # print("SSIM: {}".format(score))
                 # check if there is a difference between current img to last taken selfie.
                 # if it is the same go back to while loop
@@ -34,7 +34,7 @@ def takeSelfies(eye, smile,frame, grayscaleFrame, output_folder, cartoon, pencil
                         pencilifier.pencilMe(frame, total, output_folder)
                     else:
                         selfie_utils.take_selfie(frame, total, output_folder)
-                    last_taken_selfie = grayscaleFrame
+                    last_taken_selfie = gray
                     counter = 0
                 pass
 
@@ -49,32 +49,34 @@ def takeSelfies(eye, smile,frame, grayscaleFrame, output_folder, cartoon, pencil
                     selfie_utils.take_selfie(frame, total, output_folder)
 
                 total += 1
-                last_taken_selfie = grayscaleFrame
+                last_taken_selfie = gray
                 counter = 0
 
                 pass
 
-def detection(cascade_face, cascade_eye, cascade_smile, grayscale, img):
-    eye = None
+def detection(face_cascade, eye_cascade, smile_cascade, gray, img):
+    eyes = None
     smile = None
-    face = cascade_face.detectMultiScale(grayscale, 1.3, 4)
-    for (x_face, y_face, w_face, h_face) in face:
-        cv2.rectangle(img, (x_face, y_face), (x_face+w_face, y_face+h_face), (255, 130, 0), 2)
-        ri_grayscale = grayscale[y_face:y_face + h_face, x_face:x_face + w_face]
-        ri_color = img[y_face:y_face + h_face, x_face:x_face + w_face]
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x_face, y_face, w_face, h_face) in faces:
+        cv2.rectangle(img, (x_face, y_face), (x_face+w_face, y_face+h_face), (255, 0, 0), 2)
+        roi_gray = gray[y_face:y_face + h_face, x_face:x_face + w_face]
+        roi_color = img[y_face:y_face + h_face, x_face:x_face + w_face]
 
-        eye = cascade_eye.detectMultiScale(ri_grayscale, 1.1, 14)
-        for (x_eye, y_eye, w_eye, h_eye) in eye:
-            cv2.rectangle(ri_color, (x_eye, y_eye), (x_eye + w_eye, y_eye + h_eye), (0, 180, 60), 2)
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.03, minNeighbors=40, minSize=(30,30))
+        for (x_eye, y_eye, w_eye, h_eye) in eyes:
+            cv2.rectangle(roi_color, (x_eye, y_eye), (x_eye + w_eye, y_eye + h_eye), (0, 255, 0), 2)
 
-        smile = cascade_smile.detectMultiScale(ri_grayscale, 1.2, 15)
+
+        # add from eyes pattern
+        smile = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.2, minNeighbors=40, minSize=(30,30))
         for (x_smile, y_smile, w_smile, h_smile) in smile:
-            cv2.rectangle(ri_color, (x_smile, y_smile), (x_smile + w_smile, y_smile + h_smile), (255, 0, 130), 2)
+            cv2.rectangle(roi_color, (x_smile, y_smile), (x_smile + w_smile, y_smile + h_smile), (0, 255, 0), 2)
 
-    return img, eye, smile
+    return img, eyes, smile
 
 
-def runHaarCascade(cascade_face, cascade_eye, cascade_smile, cartoon=False, pencil=False, useCanny=False):
+def runHaarCascade(face_cascade, eye_cascade, smile_cascade, cartoon=False, pencil=False, useCanny=False):
 
     print("for regular selfies press 'n'\n"
           "for cartoon selfies press 'c'\n"
@@ -85,16 +87,18 @@ def runHaarCascade(cascade_face, cascade_eye, cascade_smile, cartoon=False, penc
 
     print("starting looking for perfect selfie mode")
 
-    vc = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
     while True:
-        _, img = vc.read()
+        ret, img = cap.read()
         origImg = img.copy()
-        grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        final, eye, smile = detection(cascade_face, cascade_eye, cascade_smile, grayscaleFrame, img)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        final, eye, smile = detection(face_cascade, eye_cascade, smile_cascade, gray, img)
         cv2.imshow('Video', final)
 
-        takeSelfies(eye, smile, origImg, grayscaleFrame, output_folder, cartoon, pencil, useCanny)
+        takeSelfies(eye, smile, origImg, gray, output_folder, cartoon, pencil, useCanny)
 
         key2 = cv2.waitKey(1) & 0xFF
         if key2 == ord('q'):
@@ -114,17 +118,17 @@ def runHaarCascade(cascade_face, cascade_eye, cascade_smile, cartoon=False, penc
             cartoon = False
 
 
-    vc.release()
+    cap.release()
     cv2.destroyAllWindows()
 
 
 
 if __name__ == '__main__':
 
-    cascade_face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    cascade_eye = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-    cascade_smile = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
     useCanny = False
 
-    runHaarCascade(cascade_face, cascade_eye, cascade_smile, useCanny=useCanny)
+    runHaarCascade(face_cascade, eye_cascade, smile_cascade, useCanny=useCanny)
